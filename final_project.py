273f469278d432fda8b67a412b0ff47681799156
midCenturyDude7 <mgriffes@gmail.com>
2020-06-04 14:11:34 -0700

    Updated top comments section

--- final_project.py
+++ final_project.py
@@ -7,13 +7,16 @@ File: final_project.py
 In its current form, this VERY BASIC program works with COVID-19 data from five countries and allows the user
 to access data based on choosing which country he/she would like to see data about.
 ***
-NEXT STEPS: (1) Provide access to all the files, likely using the OS module; (2) Include a date with each day;
+NEXT STEPS:
+(1) Provide access to all the files, likely using the OS module; STATUS: COMPLETED! Used the pathlib library
+(2) Include a date with each day / STATUS: PENDING
 (3) Data manipulation: Work with Numpy and Pandas to perform further/more detailed data manipulation such as
 cross country comparisons based on certain data points related to 'confirmed cases' or 'country with the day
-of the most confirmed cases', etc.; (4) Data visualization: Work with Matplotlib to plot the data and
-hopefully create some useful/interesting charts/graphs for the purposes of storytelling (maybe); and
+of the most confirmed cases', etc. / STATUS: PENDING
+(4) Data visualization: Work with Matplotlib to plot the data and
+hopefully create some useful/interesting charts/graphs for the purposes of storytelling (maybe) / STATUS: PENDING
 (5) Further decompose the program - likely related to #1 above - to simplify/refactor the code to make it more
-efficient and easier to read.
+efficient and easier to read / STATUS: ONGOING
 ***
 LAST BUT NOT LEAST: THANK YOU CS106A!!! What an awesome, kick-ass, amazing and super-fun experience this has been.
 Really can't say thank you enough! Everyone that contributed their time, energy and effort to this has my infinite
