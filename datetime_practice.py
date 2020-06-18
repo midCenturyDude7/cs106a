b9199b2aeeb80e6210c83d5390a52c9b16384e8f
midCenturyDude7 <mgriffes@gmail.com>
2020-06-18 16:47:50 -0700

    minor changes to ensure connection

---
+++ datetime_practice.py
@@ -0,0 +1,26 @@
+# data comes from Johns Hopkins University. Thanks to them for making this public!
+# https://github.com/CSSEGISandData/COVID-19
+# you can find data beyond cumulative cases there!
+
+"""
+File: datetime_practice.py
+Attempt to add a dateline - starting at Jan-22-20 - to each confirmed case value by working
+with one country file as an example/milestone.
+"""
+
+# Constants for the country's file that contains all confirmed cases from Jan-22 to May-09
+DATA_DIR = 'confirmed/us.txt'
+
+
+def main():
+
+    pass
+
+
+def load_us():
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
