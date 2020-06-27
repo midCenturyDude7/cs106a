e44794cdc391506267ca85143e36fc43693c239f
midCenturyDude7 <mgriffes@gmail.com>
2020-06-26 18:28:37 -0700

    Updated to fix bug for both U.S. and Brazil

--- datetime_practice.py
+++ datetime_practice.py
@@ -16,7 +16,7 @@ from datetime import datetime
 
 # Constants for the country's file that contains all confirmed cases from Jan-22 to May-09
 DATA_DIR_US = 'confirmed/us.txt'
-DATA_DIR_BRZ = 'confirmed/brazil.txt'
+DATA_DIR_BRA = 'confirmed/brazil.txt'
 DATA_DIR_MEX = 'confirmed/mexico.txt'
 
 TOTAL_CASES = 3877407
@@ -67,23 +67,27 @@ def load_us():
     country_name = 'United States'
 
     # File access and having fun with the data :)
-    filename = 'us.txt'
+    with open(DATA_DIR_US, 'r') as f:
+        country_data = f.readlines()
+        country_data_updated = []
+        for elem in country_data:
+            country_data_updated.append(elem.strip())
 
-    df = pd.read_csv(DATA_DIR_US)
-    df.columns = ['Cases']
+        df = pd.DataFrame(data=country_data_updated)
+        df.columns = ['Cases']
 
-    first_date = '1-22-20'
-    end_date = '5-09-20'
+        first_date = '1-22-20'
+        end_date = '5-9-20'
 
-    date_series = pd.date_range(start=first_date, end=end_date, freq='D')
-    df_dates = pd.DataFrame()
-    df_dates['Date'] = date_series
-    df_dates_updated = df_dates
-    df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')
+        date_series = pd.date_range(start=first_date, end=end_date, freq='D')
+        df_dates = pd.DataFrame()
+        df_dates['Date'] = date_series
+        df_dates_updated = df_dates
+        df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')
 
-    df_update = pd.concat([df_dates_updated, df], axis=1)
-    print("On " + str(df_update.at[77, 'Date']) + " there were " + str(df_update.at[77, 'Cases']) +
-          " confirmed cases of COVID-19 in the " + country_name + ".")
+        df_update = pd.concat([df_dates_updated, df], axis=1)
+        print("On " + str(df_update.at[108, 'Date']) + " there were " + str(df_update.at[108, 'Cases']) +
+              " confirmed cases of COVID-19 in the " + country_name + ".")
 
 
 def load_brazil():
@@ -91,23 +95,27 @@ def load_brazil():
     country_name = 'Brazil'
 
     # File access and having fun with the data :)
-    filename = 'brazil.txt'
+    with open(DATA_DIR_BRA, 'r') as f:
+        country_data = f.readlines()
+        country_data_updated = []
+        for elem in country_data:
+            country_data_updated.append(elem.strip())
 
-    df = pd.read_csv(DATA_DIR_BRZ)
-    df.columns = ['Cases']
+        df = pd.DataFrame(data=country_data_updated)
+        df.columns = ['Cases']
 
-    first_date = '1-22-20'
-    end_date = '5-09-20'
+        first_date = '1-22-20'
+        end_date = '5-9-20'
 
-    date_series = pd.date_range(start=first_date, end=end_date, freq='D')
-    df_dates = pd.DataFrame()
-    df_dates['Date'] = date_series
-    df_dates_updated = df_dates
-    df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')
+        date_series = pd.date_range(start=first_date, end=end_date, freq='D')
+        df_dates = pd.DataFrame()
+        df_dates['Date'] = date_series
+        df_dates_updated = df_dates
+        df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')
 
-    df_update = pd.concat([df_dates_updated, df], axis=1)
-    print("On " + str(df_update.at[77, 'Date']) + " there were " + str(df_update.at[77, 'Cases']) + 
-          " confirmed cases of COVID-19 in " + country_name + ".")
+        df_update = pd.concat([df_dates_updated, df], axis=1)
+        print("On " + str(df_update.at[108, 'Date']) + " there were " + str(df_update.at[108, 'Cases']) +
+              " confirmed cases of COVID-19 in " + country_name + ".")
 
 
 def load_mexico():
@@ -116,24 +124,24 @@ def load_mexico():
 
     # File access and having fun with the data :)
     with open(DATA_DIR_MEX, 'r') as f:
-        country_data = f.readlines()                                                # Returns a list containing lines from the file
-        country_data_updated = []                                                   # Create empty list to 'import' data into and format
-        for elem in country_data:                                                   # Iterate through the list and remove '\n' character
+        country_data = f.readlines()                    # Returns a list containing lines from the file
+        country_data_updated = []                       # Create empty list to 'import' data into and format
+        for elem in country_data:                       # Iterate through the list and remove '\n' character
             country_data_updated.append(elem.strip())
 
-        df = pd.DataFrame(data=country_data_updated)                                # Create a dataframe with pandas
-        df.columns = ['Cases']                                                      # Assign column label
-        
-        first_date = '1-22-20'                                                      # Date range variable - start date
-        end_date = '5-09-20'                                                        # Date range variable - end date
+        df = pd.DataFrame(data=country_data_updated)    # Create a dataframe with pandas
+        df.columns = ['Cases']                          # Assign column label
 
-        date_series = pd.date_range(start=first_date, end=end_date, freq='D')       # Assign date range to date column with date range variables
-        df_dates = pd.DataFrame()                                                   # Create dataframe
-        df_dates['Date'] = date_series                                              # Assign column label and assign data range variable to it
-        df_dates_updated = df_dates                                                 # Create new dataframe to format datetime stamp
-        df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')         # Remove timestamp and format date to follow Jan-01-20
+        first_date = '1-22-20'                          # Date range variable - start date
+        end_date = '5-9-20'                            # Date range variable - end date
 
-        df_update = pd.concat([df_dates_updated, df], axis=1)                       # Concatenate the dataframes to combine 'Date' and 'Cases' columns into one dataframe
+        date_series = pd.date_range(start=first_date, end=end_date, freq='D')   # Assign date range to date column with date range variables
+        df_dates = pd.DataFrame()                                               # Create dataframe
+        df_dates['Date'] = date_series                                          # Assign column label and assign data range variable to it
+        df_dates_updated = df_dates                                             # Create new dataframe to format datetime stamp
+        df_dates_updated['Date'] = df_dates['Date'].dt.strftime('%b-%d-%Y')     # Remove timestamp and format date to follow Jan-01-20
+
+        df_update = pd.concat([df_dates_updated, df], axis=1)                   # Concatenate the dataframes to combine 'Date' and 'Cases' columns into one dataframe
         
         # Print result to console
         print("On " + str(df_update.at[108, 'Date']) + " there were " + str(df_update.at[108, 'Cases']) + 
